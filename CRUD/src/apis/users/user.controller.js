125e6e12788c0b5106393232df664eee546fe893
hoangvtiendat2004 <161110799+hoangvtiendat2004@users.noreply.github.com>
2024-05-26 16:01:31 +0700

    anhToan

--- CRUD/src/apis/users/user.controller.js
+++ CRUD/src/apis/users/user.controller.js
@@ -1,11 +1,11 @@
 import UserService from './user.service';
 class UserController {
-  getAll(req, res, next) {
-    // return res.send("hello");
-    const users = UserService.getUsers();
-    console.log('vao day controller');
-    return res.json(users, htmlTemplate);
-  }
+      getAll(req, res, next) {
+        // return res.send("hello");
+        const users = UserService.getUsers()
+        console.log("vao day controller")
+        return res.json(users, htmlTemplate)
+    }
 }
 
-export default UserController = new UserController();
+export default UserController = new UserController()
--- CRUD/src/apis/users/user.servicer.js
+++ CRUD/src/apis/users/user.servicer.js
@@ -1,13 +1,13 @@
-import fs from 'fs';
+import fs from 'fs'
 
 class UserService {
-  getUsers() {
-    // read user.json with fs
-    console.log('vao day service');
-    const users = fs.readFileSync('./data.json', 'utf-8');
-    console.log('users ', users);
-    return users;
-  }
+    getUsers() {
+        // read user.json with fs
+        console.log("vao day service")
+        const users = fs.readFileSync("./data.json", 'utf-8')
+        console.log("users ", users)
+        return users;
+    }
 }
 
 export default new UserService();
